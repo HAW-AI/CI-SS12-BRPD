5374c053b0198bbfd00193829f9a7a289cfd591c
Ben <salzig+github@gmail.com>
2012-04-29 19:57:27 +0200

    print builded ast

--- haw/ci/Application.java
+++ haw/ci/Application.java
@@ -8,12 +8,12 @@ import java.io.Reader;
 import haw.ci.lib.ITokenStream;
 import haw.ci.lib.Parser;
 import haw.ci.lib.Tokenizer;
-import haw.ci.lib.Yytoken;
 
 public class Application {
 	public static void main(String[] args) throws IOException {
 		Reader fileReader = new FileReader(new File("./example.ob"));
 		ITokenStream tokenStream = new Tokenizer(fileReader);
 		Parser parser = new Parser(tokenStream);
+		System.out.println(parser.build());
 	}
 }
