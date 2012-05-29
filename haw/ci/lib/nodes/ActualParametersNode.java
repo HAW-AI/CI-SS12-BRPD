3955353e20ae7d59875ec5028211d7e474eda9a7
Ben <salzig+github@gmail.com>
2012-05-29 21:47:22 +0200

    actualParametersNode impl

--- haw/ci/lib/nodes/ActualParametersNode.java
+++ haw/ci/lib/nodes/ActualParametersNode.java
@@ -1,10 +1,15 @@
 package haw.ci.lib.nodes;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ActualParametersNode extends AbstractNode {
+	private static final long serialVersionUID = 4324447615915518226L;
+
+	private List<AbstractNode> nodes = new ArrayList<AbstractNode>();
 
 	public void add(AbstractNode expression) {
-		// TODO Auto-generated method stub
-		
+		nodes.add(expression);
 	}
 
 }
