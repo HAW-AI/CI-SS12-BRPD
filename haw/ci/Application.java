947644f4571be7f5caad6836428a0730674e0bc2
Ben <salzig+github@gmail.com>
2012-05-29 22:45:33 +0200

    add SymTaG call

--- haw/ci/Application.java
+++ haw/ci/Application.java
@@ -8,15 +8,21 @@ import java.io.Reader;
 import haw.ci.lib.ITokenStream;
 import haw.ci.lib.Parser;
 import haw.ci.lib.ParserAcceptError;
+import haw.ci.lib.SymTaG;
 import haw.ci.lib.Tokenizer;
+import haw.ci.lib.nodes.AbstractNode;
 
 public class Application {
 	public static void main(String[] args) throws IOException {
 		Reader fileReader = new FileReader(new File("./example5.ob"));
 		ITokenStream tokenStream = new Tokenizer(fileReader);
 		Parser parser = new Parser(tokenStream);
+		AbstractNode node;
 		try {
-			System.out.println(parser.build());
+			node = parser.build();
+			System.out.println(node);
+			
+			SymTaG.build(node);
 		} catch (ParserAcceptError e) {
 			System.out.println(String.format("Matrue Failure (cur: %s, nex: %s)", parser.getCurrent(),parser.getNext()));
 			e.printStackTrace();
