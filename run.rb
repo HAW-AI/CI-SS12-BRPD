2bc3ae57bd5432ac3c9af6046d8dec2469e166c9
Ben <salzig+github@gmail.com>
2012-06-06 17:34:27 +0200

    nice and simple run script

---
+++ run.rb
@@ -0,0 +1,12 @@
+#!/usr/bin/env ruby
+#
+if ARGV[1].nil? 
+  file = "TestRun.txt"
+  debug = 1
+else
+  file = ARGV[0]
+  debug = ARGV[1] || 0
+end
+puts "File: #{file}"
+puts `java -jar NewOBI.jar #{file} #{debug}`
+
--- run.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/bash
-java -jar NewOBI.jar TestRun.txt 0
