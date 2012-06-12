d0ab1fd8e07e5fe6a967fca074964aead4edb29e
Patrick Detlefsen <patrick.detlefsen@gmail.com>
2012-06-12 23:14:55 +0200

    compile for expressions

--- haw/ci/lib/nodes/ExpressionNode.java
+++ haw/ci/lib/nodes/ExpressionNode.java
@@ -1,6 +1,9 @@
 package haw.ci.lib.nodes;
 
+import sun.org.mozilla.javascript.internal.Token;
+import haw.ci.lib.SymbolTable;
 import haw.ci.lib.Tokens;
+import haw.ci.lib.descriptor.Descriptor;
 
 public class ExpressionNode extends AbstractNode {
 	private static final long serialVersionUID = 1L;
@@ -66,6 +69,43 @@ public class ExpressionNode extends AbstractNode {
 	    return result;
 	}
 
+	public Descriptor compile(SymbolTable symbolTable) {
+		left.compile(symbolTable);
+
+		if (left instanceof IdentNode) {
+			write("CONT, 1");
+		}
+		right.compile(symbolTable);
+
+		if (right instanceof IdentNode) {
+			write("CONT, 1");
+		}
+
+		if (binOpToken.equals(Tokens.MATH_ADD)) {
+			write("ADD");
+		} else if (binOpToken.equals(Tokens.MATH_SUB)) {
+			write("SUB");
+		} else if (binOpToken.equals(Tokens.MATH_MUL)) {
+			write("MUL");
+		} else if (binOpToken.equals(Tokens.MATH_DIV)) {
+			write("DIV");
+		} else if (binOpToken.equals(Tokens.EQUAL)) {
+			write("EQ");
+		} else if (binOpToken.equals(Tokens.MORE)) {
+			write("GT");
+		} else if (binOpToken.equals(Tokens.LESS)) {
+			write("LT");
+		} else if (binOpToken.equals(Tokens.MORE_EQUAL)) {
+			write("GE");
+		} else if (binOpToken.equals(Tokens.LESS_EQUAL)) {
+			write("LE");
+		} else if (binOpToken.equals(Tokens.NOT_EQUAL)) {
+			write("NEQ");
+		} else {
+			write("UNEXPECTED SYMBOL IN: ExpressionNode");
+		}
+		return null;
+	}
 
 	
 }
