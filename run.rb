e1cece8de13269237ef94dd067766d40a6c05709
Ben <salzig+github@gmail.com>
2012-06-16 13:44:03 +0200

    add variant for debug switch

--- run.rb
+++ run.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 #
-if ARGV[1].nil? 
+if ARGV[0].nil? && ARGV[1].nil?
   file = "TestRun.txt"
   debug = 1
+elsif !ARGV[0].nil? && ARGV[1].nil?
+  file = "TestRun.txt"
+  debug = ARGV[0] || 0
 else
   file = ARGV[0]
   debug = ARGV[1] || 0
